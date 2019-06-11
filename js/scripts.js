3efa8e5536209573670ef111ecfe45f7b593e021
Alex Siegberg and Micah Zaayer <micah@>
2019-06-11 10:17:27 -0700

    refactor business logic from user interface, fix indentation.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,19 @@
 $(document).ready(function(){
-var vowels = ["a", "e", "i", "o", "u"]
-$("#submitButton").click(function() {
-  var userInput = $("#userInput").val();
-  var newSentence = userInput.split("");
-  for(var letter = 0; letter < newSentence.length; letter++) {
-    for(vowel = 0; vowel < vowels.length; vowel++) {
-      if(newSentence[letter] === vowels[vowel]) {
-        //console.log(newSentence[letter] + " is the same as " + vowels[vowel]);
-        newSentence[letter] = "-";
+  //back-end
+  var vowels = ["a", "e", "i", "o", "u"]
+  $("#submitButton").click(function() {
+    var userInput = $("#userInput").val();
+    var newSentence = userInput.split("");
+    for(var letter = 0; letter < newSentence.length; letter++) {
+      for(vowel = 0; vowel < vowels.length; vowel++) {
+        if(newSentence[letter] === vowels[vowel]) {
+          newSentence[letter] = "-";
+        }
       }
     }
-  }
-  console.log(userInput);
-  var finalSentence = newSentence.join('')
-  $(".output").text(finalSentence);
-  console.log(newSentence);
-})
+    var finalSentence = newSentence.join('')
+
+    //front-end
+    $(".output").text(finalSentence);
+  });
 });
